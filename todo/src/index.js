c0d4d4c2c1e41064e76ecad0e7c708b56d61c9b6
Zolton <gmzesq@gmail.com>
2019-06-17 16:50:35 -0400

    index page set up, not working

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const initialState = {
+    todos: [
+        {value: "take out garbage",
+        completed: false},
+        {value: "water plants",
+        completed: false},
+        {value: "shop for food",
+        completed: false},
+    ]
+}
+
+const reducer = (state=initialState, action) => {
+    switch(action.type) {
+        default: return state
+    }
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
