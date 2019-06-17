788104c1ee3e28b2f76b5bc486eeaa41dfa322f2
Zolton <gmzesq@gmail.com>
2019-06-17 16:59:28 -0400

    Got the initialState set up and a dummy reducer.  Good enough for today, I'll leave tomororow's problems to tomorrow's me

---
+++ todo/src/components/todoList.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+export default class todoList extends React.Component {
+    
+    render(){
+        return(
+            <>
+            Hello from todoList!
+            </>
+        )
+    }
+}
